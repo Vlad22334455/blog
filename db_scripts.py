355c70e613313ed4fde3869cf76b5d48af981d60
Vlad <vladdavymuka22@gmail.com>
2024-02-16 20:15:55 +0200

    lesson 16.02

---
+++ db_scripts.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+connection = None
+cursor = None
+
+def open():
+    global connection, cursor
+    connection = sqlite3.connect('blog.db')
+    cursor = connection.cursor()
+
+def close():
+    cursor.close()
+    connection.close()
+
