024623dd947361b8c6e2a93aef893f9e0babecab
Vlad22334455 <127614851+Vlad22334455@users.noreply.github.com>
2024-02-16 20:07:22 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, url_for, redirect
+import settings as stg 
+
+app = Flask(__name__, static_folder=stg.PATH_STATIC)
+app.config['SECRET_KEY'] = stg.SECRET_KEY 
+
+@app.route('/')
+def index ():
+    url = url_for('static','templates/index.html') 
+    return redirect(url)
+
+@app.route('/post/category')
+def post_category():
+    return 'Categories of posts'
+
+@app.route('/post/view')
+def post_view():
+    return 'here will be post'
+
+@app.route('/about')
+def about():
+    return 'information about Athor'
+
+app.run(debug=True)
---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+PATH = os.path.dirname(__file__) + os.sep
+PATH_STATIC = PATH + '/static' + os.sep
+
+SECRET_KEY = 'DD4EE7C57F198E77BF9A4BF584F5B'
